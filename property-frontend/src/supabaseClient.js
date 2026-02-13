ead39bc6e41f67949970a1294e694132449161d2
JosKavi33 <124068596+JosKavi33@users.noreply.github.com>
2026-02-12 19:40:08 -0500

    Update supabaseClient.js

--- property-frontend/src/supabaseClient.js
+++ property-frontend/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://kxyushotafmxiygjimvl.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4eXVzaG90YWZteGl5Z2ppbXZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyNDI0MDIsImV4cCI6MjA4NTgxODQwMn0.tkNRx-rWeT5YS1Q6XhUlzP4liWcW53WgFYBJ06raI_4";
+const SUPABASE_ANON_KEY = "";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
